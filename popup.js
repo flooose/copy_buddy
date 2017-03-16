e1be61a7137ba48f38d8eb0c487767a5fec1320f
christopher floess <chris.floess@jjff.de>
2017-03-16 18:13:28 +0100

    [WIP] basic editing of fields

--- popup.js
+++ popup.js
@@ -13,16 +13,51 @@
  *   GNU General Public License for more details.
  */
 
-var startApp = function(){
-    type0 = document.getElementById('type0');
-    type1 = document.getElementById('type1');
-    type2 = document.getElementById('type2');
-    type3 = document.getElementById('type3');
+function save_options() {
+    var copyFields = {};
+    copyFields.type0 = document.getElementById('type0').textContent;
+    copyFields.type1 = document.getElementById('type1').textContent;
+    copyFields.type2 = document.getElementById('type2').textContent;
+    copyFields.type3 = document.getElementById('type3').textContent;
+
+    chrome.storage.sync.set({
+        copyFields: copyFields
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+function startApp(){
+    let input = document.getElementById('copy-input');
+    let type0 = document.getElementById('type0');
+    let type1 = document.getElementById('type1');
+    let type2 = document.getElementById('type2');
+    let type3 = document.getElementById('type3');
     const copyFields = chrome.storage.sync.get(null, function({ copyFields }) {
         type0.textContent = copyFields.type0;
         type1.textContent = copyFields.type1;
         type2.textContent = copyFields.type2;
         type3.textContent = copyFields.type3;
+    });
+
+    input.addEventListener('keypress', function(event){
+        if (event.keyCode === 13) {
+            if(this.value !== "") {
+                let dataFor = this.getAttribute('data-for');
+                if( dataFor !== undefined) {
+                    let element = document.getElementById(dataFor)
+                    element.textContent = this.value;
+                    save_options();
+                    element.setAttribute('style', "display: block");
+                    this.value="";
+                }
+            }
+        }
     });
 
     [type0, type1, type2, type3].forEach(function(type){
@@ -38,6 +73,16 @@ var startApp = function(){
             } catch(err) {
                 console.log('Oops, unable to cut');
             }
+        }.bind(this));
+        type.addEventListener('dblclick', function(event) {
+            let input = document.getElementById('copy-input');
+            let target = event.target;
+
+            input.value = target.textContent;
+            input.focus();
+            input.setAttribute('data-for', target.id);
+            target.setAttribute('style', "display: none");
+
         }.bind(this));
     });
 }
