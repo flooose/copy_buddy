848badd113c4aa48396db06551990795067116ee
christopher floess <chris.floess@jjff.de>
2018-03-04 13:56:45 -0500

    Introduce Angular

--- karma.conf.js
+++ karma.conf.js
@@ -1,88 +1,33 @@
-// Karma configuration
-// Generated on Fri Apr 14 2017 14:52:02 GMT+0200 (CEST)
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-        'spec/**/*[sS]pec.js',
-        'copy_buddy/*.js'
+    frameworks: ['jasmine', '@angular/cli'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular/cli/plugins/karma')
     ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-
-
-      // test results reporter to use
-      // possible values: 'dots', 'progress'
-      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-      reporters: ['progress'],
-
-
-      // web server port
-      port: 9876,
-
-
-      // enable / disable colors in the output (reporters and logs)
-      colors: true,
-
-
-      // level of logging
-      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-      logLevel: config.LOG_INFO,
-
-
-      // enable / disable watching file and executing tests whenever any file changes
-      autoWatch: true,
-
-
-      // start these browsers
-      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-      browsers: ['PhantomJS', 'Chrome', 'Firefox'],
-
-
-      // Continuous Integration mode
-      // if true, Karma captures browsers, runs the tests and exits
-      singleRun: false,
-
-      // Concurrency level
-      // how many browser should be started simultaneous
-      concurrency: Infinity
-
-    });
-
-    if(process.env.TRAVIS){
-        var customLaunchers = {
-
-            customLaunchers: {
-                Chrome_travis_ci: {
-                    base: 'Chrome',
-                    flags: ['--no-sandbox']
-                }
-            }
-        };
-
-        config.set(
-            Object.assign(config,
-                          customLaunchers,
-                          { browsers: ['PhantomJS'] }));
-    }
-}
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  });
+};
